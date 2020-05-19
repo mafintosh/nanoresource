373f734b1e414bd96ec76b488aea02594cc6f656
Martin Acosta <tinchoz49@gmail.com>
2020-05-19 15:08:49 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,108 +1,128 @@
+const preopening = Symbol('opening when closing')
 const opening = Symbol('opening queue')
 const preclosing = Symbol('closing when inactive')
 const closing = Symbol('closing queue')
 const sync = Symbol('sync')
 const fastClose = Symbol('fast close')
+const reopen = Symbol('allow reopen')
+const init = Symbol('init state')
 
-module.exports = Nanoresource
+class Nanoresource {
+  constructor (opts) {
+    if (!opts) opts = {}
+    if (opts.open) this._open = opts.open
+    if (opts.close) this._close = opts.close
 
-function Nanoresource (opts) {
-  if (!(this instanceof Nanoresource)) return new Nanoresource(opts)
+    this[init]()
 
-  if (!opts) opts = {}
-  if (opts.open) this._open = opts.open
-  if (opts.close) this._close = opts.close
-
-  this.opening = false
-  this.opened = false
-  this.closing = false
-  this.closed = false
-  this.actives = 0
-
-  this[opening] = null
-  this[preclosing] = null
-  this[closing] = null
-  this[sync] = false
-  this[fastClose] = true
-}
-
-Nanoresource.prototype._open = function (cb) {
-  cb(null)
-}
-
-Nanoresource.prototype._close = function (cb) {
-  cb(null)
-}
-
-Nanoresource.prototype.open = function (cb) {
-  if (!cb) cb = noop
-
-  if (this[closing] || this.closed) return process.nextTick(cb, new Error('Resource is closed'))
-  if (this.opened) return process.nextTick(cb)
-
-  if (this[opening]) {
-    this[opening].push(cb)
-    return
+    this[reopen] = opts.reopen || false
+    this[preopening] = null
+    this[opening] = null
+    this[preclosing] = null
+    this[closing] = null
+    this[sync] = false
+    this[fastClose] = true
   }
 
-  this.opening = true
-  this[opening] = [cb]
-  this[sync] = true
-  this._open(onopen.bind(this))
-  this[sync] = false
-}
-
-Nanoresource.prototype.active = function (cb) {
-  if ((this[fastClose] && this[preclosing]) || this[closing] || this.closed) {
-    if (cb) process.nextTick(cb, new Error('Resource is closed'))
-    return false
+  [init] () {
+    this.opening = false
+    this.opened = false
+    this.closing = false
+    this.closed = false
+    this.actives = 0
   }
-  this.actives++
-  return true
-}
 
-Nanoresource.prototype.inactive = function (cb, err, val) {
-  if (!--this.actives) {
-    const queue = this[preclosing]
-    if (queue) {
-      this[preclosing] = null
-      while (queue.length) this.close(queue.shift())
+  _open (cb) {
+    cb(null)
+  }
+
+  _close (cb) {
+    cb(null)
+  }
+
+  open (cb) {
+    if (!cb) cb = noop
+
+    if (this.closing || this.closed) {
+      if (!this[reopen]) {
+        return process.nextTick(cb, new Error('Resource is closed'))
+      }
+
+      if (this.closing) {
+        if (!this[preopening]) this[preopening] = []
+        this[preopening].push(cb)
+        return
+      }
+
+      this[init]()
     }
+
+    if (this.opened) return process.nextTick(cb)
+
+    if (this[opening]) {
+      this[opening].push(cb)
+      return
+    }
+
+    this.opening = true
+    this[opening] = [cb]
+    this[sync] = true
+    this._open(onopen.bind(this))
+    this[sync] = false
   }
 
-  if (cb) cb(err, val)
-}
-
-Nanoresource.prototype.close = function (allowActive, cb) {
-  if (typeof allowActive === 'function') return this.close(false, allowActive)
-  if (!cb) cb = noop
-
-  if (allowActive) this[fastClose] = false
-
-  if (this.closed) return process.nextTick(cb)
-
-  if (this.actives || this[opening]) {
-    if (!this[preclosing]) this[preclosing] = []
-    this[preclosing].push(cb)
-    return
+  active (cb) {
+    if ((this[fastClose] && this[preclosing]) || this[closing] || this.closed) {
+      if (cb) process.nextTick(cb, new Error('Resource is closed'))
+      return false
+    }
+    this.actives++
+    return true
   }
 
-  if (!this.opened) {
-    this.closed = true
-    process.nextTick(cb)
-    return
+  inactive (cb, err, val) {
+    if (!--this.actives) {
+      const queue = this[preclosing]
+      if (queue) {
+        this[preclosing] = null
+        while (queue.length) this.close(queue.shift())
+      }
+    }
+
+    if (cb) cb(err, val)
   }
 
-  if (this[closing]) {
-    this[closing].push(cb)
-    return
-  }
+  close (allowActive, cb) {
+    if (typeof allowActive === 'function') return this.close(false, allowActive)
+    if (!cb) cb = noop
 
-  this.closing = true
-  this[closing] = [cb]
-  this[sync] = true
-  this._close(onclose.bind(this))
-  this[sync] = false
+    if (allowActive) this[fastClose] = false
+
+    if (this.closed) return process.nextTick(cb)
+
+    if (this.actives || this[opening]) {
+      if (!this[preclosing]) this[preclosing] = []
+      this[preclosing].push(cb)
+      return
+    }
+
+    if (!this.opened) {
+      this.closed = true
+      process.nextTick(cb)
+      return
+    }
+
+    if (this[closing]) {
+      this[closing].push(cb)
+      return
+    }
+
+    this.closing = true
+    this[closing] = [cb]
+    this[sync] = true
+    this._close(onclose.bind(this))
+    this[sync] = false
+  }
 }
 
 function onopen (err) {
@@ -129,6 +149,15 @@ function onclose (err) {
   this[closing] = null
   this.closed = !err
   while (queue.length) queue.shift()(err)
+
+  const cqueue = this[preopening]
+  if (cqueue) {
+    this[preopening] = null
+    while (cqueue.length) this.open(cqueue.shift())
+  }
 }
 
 function noop () {}
+
+module.exports = (opts) => new Nanoresource(opts)
+module.exports.Nanoresource = Nanoresource
--- test/basic.js
+++ test/basic.js
@@ -1,116 +1,166 @@
 const tape = require('tape')
 const nanoresource = require('../')
+const nanoresourceEmitter = require('../emitter')
 
-tape('basic usage', function (t) {
-  t.plan(2 + 2 + 3 * 3 + 3 * 3)
+function test (title, createNanoresource) {
+  tape(title + ' basic usage', function (t) {
+    t.plan(2 + 2 + 3 * 3 + 3 * 3)
 
-  let opened = false
-  let closed = false
+    let opened = false
+    let closed = false
 
-  const r = nanoresource({
-    open (cb) {
-      t.notOk(closed, 'not closed in open')
-      t.notOk(opened, 'only open once')
-      opened = true
-      cb(null)
-    },
-    close (cb) {
-      t.ok(opened, 'opened when closing')
-      t.notOk(closed, 'only close once')
-      closed = true
-      cb(null)
-    }
-  })
-
-  r.open(onopen)
-  r.open(onopen)
-  r.open(onopen)
-
-  r.close(onclose)
-  r.close(onclose)
-  r.close(onclose)
-
-  function onopen (err) {
-    t.error(err, 'no error')
-    t.ok(r.opened, 'was opened')
-    t.ok(opened, 'open ran')
-  }
-
-  function onclose (err) {
-    t.error(err, 'no error')
-    t.ok(r.closed, 'was closed')
-    t.ok(closed, 'close ran')
-  }
-})
-
-tape('open/close is never sync', function (t) {
-  const r = nanoresource({
-    open (cb) {
-      cb(null)
-    },
-    close (cb) {
-      cb(null)
-    }
-  })
-
-  let syncOpen = true
-  r.open(function () {
-    t.notOk(syncOpen)
-    let syncClose = true
-    r.close(function () {
-      t.notOk(syncClose)
-      t.end()
+    const r = createNanoresource({
+      open (cb) {
+        t.notOk(closed, 'not closed in open')
+        t.notOk(opened, 'only open once')
+        opened = true
+        cb(null)
+      },
+      close (cb) {
+        t.ok(opened, 'opened when closing')
+        t.notOk(closed, 'only close once')
+        closed = true
+        cb(null)
+      }
     })
-    syncClose = false
-  })
-  syncOpen = false
-})
 
-tape('active/inactive', function (t) {
-  t.plan(2 + 10 * 1)
+    r.open(onopen)
+    r.open(onopen)
+    r.open(onopen)
 
-  let fails = 0
-  let ran = 0
+    r.close(onclose)
+    r.close(onclose)
+    r.close(onclose)
 
-  const r = nanoresource({
-    close (cb) {
-      t.same(fails, 0)
-      t.same(ran, 10)
-      cb(null)
+    function onopen (err) {
+      t.error(err, 'no error')
+      t.ok(r.opened, 'was opened')
+      t.ok(opened, 'open ran')
+    }
+
+    function onclose (err) {
+      t.error(err, 'no error')
+      t.ok(r.closed, 'was closed')
+      t.ok(closed, 'close ran')
     }
   })
 
-  for (let i = 0; i < 10; i++) {
+  tape(title + ' open/close is never sync', function (t) {
+    const r = createNanoresource({
+      open (cb) {
+        cb(null)
+      },
+      close (cb) {
+        cb(null)
+      }
+    })
+
+    let syncOpen = true
     r.open(function () {
-      t.ok(r.active())
-      ran++
-      fails++
-      setImmediate(function () {
-        fails--
-        r.inactive()
+      t.notOk(syncOpen)
+      let syncClose = true
+      r.close(function () {
+        t.notOk(syncClose)
+        t.end()
       })
+      syncClose = false
     })
-  }
-
-  r.open(() => r.close())
-})
-
-tape('active after close', function (t) {
-  t.plan(5)
-
-  const r = nanoresource({
-    close (cb) {
-      t.notOk(r.active(), 'cannot be active in close')
-      setImmediate(cb)
-    }
+    syncOpen = false
   })
 
-  t.ok(r.active(), 'can be active')
-  r.open()
-  t.ok(r.active(), 'can be active')
-  r.close(() => t.notOk(r.active(), 'still cannot be active'))
-  t.notOk(r.active(), 'cannot be active cause close was call')
+  tape(title + ' active/inactive', function (t) {
+    t.plan(2 + 10 * 1)
 
-  r.inactive()
-  r.inactive()
-})
+    let fails = 0
+    let ran = 0
+
+    const r = createNanoresource({
+      close (cb) {
+        t.same(fails, 0)
+        t.same(ran, 10)
+        cb(null)
+      }
+    })
+
+    for (let i = 0; i < 10; i++) {
+      r.open(function () {
+        t.ok(r.active())
+        ran++
+        fails++
+        setImmediate(function () {
+          fails--
+          r.inactive()
+        })
+      })
+    }
+
+    r.open(() => r.close())
+  })
+
+  tape(title + ' active after close', function (t) {
+    t.plan(5)
+
+    const r = createNanoresource({
+      close (cb) {
+        t.notOk(r.active(), 'cannot be active in close')
+        setImmediate(cb)
+      }
+    })
+
+    t.ok(r.active(), 'can be active')
+    r.open()
+    t.ok(r.active(), 'can be active')
+    r.close(() => t.notOk(r.active(), 'still cannot be active'))
+    t.notOk(r.active(), 'cannot be active cause close was call')
+
+    r.inactive()
+    r.inactive()
+  })
+
+  tape(title + ' reopen', function (t) {
+    t.plan(2 * 2 + 2 + 3 * 2 + 3)
+
+    let opened = false
+    let closed = false
+
+    const r = createNanoresource({
+      reopen: true,
+      open (cb) {
+        t.notOk(closed, 'not closed in open')
+        t.notOk(opened, 'only open once')
+        opened = true
+        cb(null)
+      },
+      close (cb) {
+        t.ok(opened, 'opened when closing')
+        t.notOk(closed, 'only close once')
+        closed = true
+        setImmediate(() => {
+          r.open(onopen)
+          cb(null)
+        })
+      }
+    })
+
+    r.open(onopen)
+
+    r.close(onclose)
+
+    function onopen (err) {
+      t.error(err, 'no error')
+      t.ok(r.opened, 'was opened')
+      t.ok(opened, 'open ran')
+    }
+
+    function onclose (err) {
+      t.error(err, 'no error')
+      t.ok(r.closed, 'was closed')
+      t.ok(closed, 'close ran')
+      opened = false
+      closed = false
+    }
+  })
+}
+
+test('nanoresource', nanoresource)
+test('nanoresource/emitter', nanoresourceEmitter)
